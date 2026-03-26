b88b8cf34d50821d8a488c091dde10883153644b
merlinsalomi00-beep <merlinsalomi00@gmail.com>
2026-03-26 22:34:34 +0530

    UC-5 Completed

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
-package org.example;
+public class HelloApp {
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String name = "World";
+        if (args.length > 0) {
+
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+
+            for (String arg : args) {
+
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+
+                nameBuilder.append(arg);
+                first = false;
+            }
+
+            name = nameBuilder.toString();
         }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
